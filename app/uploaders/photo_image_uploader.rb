fabc33922fcfa8eaca5172e9a1b29836f5e92861
NeilMcGuire <nmcguire61@gmail.com>
2015-11-11 16:36:48 +0000

    doing serious cssing

--- app/uploaders/photo_image_uploader.rb
+++ app/uploaders/photo_image_uploader.rb
@@ -8,8 +8,9 @@ class PhotoImageUploader < CarrierWave::Uploader::Base
   end
 
   version :medium do
-    process :resize_to_fit => [400, 400]
+    process :resize_to_fill => [400, 400]
   end
+
 
   storage :file
  
