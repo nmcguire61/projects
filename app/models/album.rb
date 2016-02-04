4ab6276dd6ea0576deae35e2c40aaea81b8c8669
NeilMcGuire <nmcguire61@gmail.com>
2016-02-04 22:07:40 +0000

    tidying up css

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,11 +1,11 @@
 class Album < ActiveRecord::Base
 
-  IMAGE_SIZES = {
-    # :thumb => [200, 200],
-    :medium => [800, 800],
-  }
+  # IMAGE_SIZES = {
+  #   # :thumb => [200, 200],
+  #   :medium => [800, 800],
+  # }
   
-  mount_uploader :photo_image, PhotoImageUploader
+  # mount_uploader :photo_image, PhotoImageUploader
   belongs_to :user
   has_many :photos
 end
